a3ab287f21f19b23a9e8770e50ae7c28c8e0ea21
luishparedes <luishparedes94@gmail.com>
2026-03-12 12:44:37 -0400

    cambio de js/taximoto.js

--- js/taximoto.js
+++ js/taximoto.js
@@ -1,6 +1,6 @@
 // Configuración de Supabase
 const SUPABASE_URL = 'https://kamcozmlzgvixaopsiqk.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_HJkELm1PKc9hTB7R8DsRng_a2qqkv8z';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImthbWNvem1semd2aXhhb3BzaXFrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMxNjY1NTYsImV4cCI6MjA4ODc0MjU1Nn0.oCdHd4mPEMMhctsCNcviXLFuwrDuLSym5raTmTtUtGQ';
 
 // Inicializar cliente de Supabase (v2) - usando la variable global 'supabase' del CDN
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
